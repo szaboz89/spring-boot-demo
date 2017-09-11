cd0579f3b2511f68099d3dbe22c22c0e6bb3ebd6
Zoltán Szabó <zoltan.szabo@atolldev.hu>
2017-09-11 21:58:14 +0200

    configure MySql database

--- spring-boot-demo-web/src/main/java/com/szabodev/demo/DemoApplication.java
+++ spring-boot-demo-web/src/main/java/com/szabodev/demo/DemoApplication.java
@@ -1,8 +1,6 @@
 package com.szabodev.demo;
 
-import com.szabodev.demo.dto.DeveloperDTO;
 import com.szabodev.demo.dto.SkillDTO;
-import com.szabodev.demo.service.DeveloperService;
 import com.szabodev.demo.service.SkillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -12,22 +10,16 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-
 @EnableJpaRepositories
 @EntityScan
 @ComponentScan
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
-    private final DeveloperService developerService;
     private final SkillService skillService;
 
     @Autowired
-    public DemoApplication(DeveloperService developerService, SkillService skillService) {
-        this.developerService = developerService;
+    public DemoApplication(SkillService skillService) {
         this.skillService = skillService;
     }
 
@@ -38,18 +30,17 @@ public class DemoApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        SkillDTO javascript = new SkillDTO("javascript", "Javascript language skill");
-        SkillDTO java = new SkillDTO("java", "Java language skill");
-        SkillDTO html = new SkillDTO("html", "HTML skill");
-        SkillDTO css = new SkillDTO("css", "CSS skill");
+        if (skillService.findAll().size() == 0) {
+            SkillDTO javascript = new SkillDTO("javascript", "Javascript language skill");
+            SkillDTO java = new SkillDTO("java", "Java language skill");
+            SkillDTO html = new SkillDTO("html", "HTML skill");
+            SkillDTO css = new SkillDTO("css", "CSS skill");
 
-        javascript = skillService.save(javascript);
-        java = skillService.save(java);
-        html = skillService.save(html);
-        css = skillService.save(css);
+            skillService.save(javascript);
+            skillService.save(java);
+            skillService.save(html);
+            skillService.save(css);
+        }
 
-        List<DeveloperDTO> developers = new LinkedList<>();
-        developers.add(new DeveloperDTO("Mark", "Taylor", "mark.taylor@example.com", Arrays.asList(javascript, java, html)));
-        developerService.saveAll(developers);
     }
 }
